6f4f2ca2c808dcc33dc5a9cfce96796592b89d93
Phan Hùng Phong <phanhungphong0406@users.noreply.github.com>
2019-12-27 08:04:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ global $db;
     <li   >
       <form style="padding-bottom: 20px; padding-left: 20px;" class="form-inline mt-2 mt-md-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name = "Serch" >Search</button>
       </form>
       <li>
     <li>
